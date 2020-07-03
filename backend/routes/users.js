4f63df4c3a5553dc05872fdce34d1dc18549925b
BrandonBroomell <bbroom2222@gmail.com>
2020-07-03 15:21:07 -0400

    minor change to spelling and test database added in .env file

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-let User = require('../mmodels/user.model');
+let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
     User.find()
