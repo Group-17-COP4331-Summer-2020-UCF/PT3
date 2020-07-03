5cab9aad32eb504e2f45286658933a930bd41be2
BrandonBroomell <bbroom2222@gmail.com>
2020-07-03 18:01:30 -0400

    searchUsers changed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
-router.route('/').get((req, res) => {
-    User.find()
+router.route('/searchUser').get((req, res) => {
+    var query = { username: req.body.username};
+    User.find(query)
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route('/addUser').post((req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
