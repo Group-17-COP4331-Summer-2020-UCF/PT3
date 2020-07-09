2cb0c91c450a9bb64b2c88166ad194309664c73e
JaimeBR-ui <bambas@knights.ucf.edu>
2020-07-08 20:09:35 -0400

    Moved frontend inside api to simplify deployment.

--- backend/frontend/src/components/Tour.js
+++ backend/frontend/src/components/Tour.js
@@ -5,7 +5,7 @@ function Tour()
     return(
         // <p1> placeholder for image slideshow</p1>
         <div id="tourDiv" className='col-md-10'>
-            <img id='homeDisplay' alt="world" src="https://raw.githubusercontent.com/Group-17-COP4331-Summer-2020-UCF/PT3v2/master/img/Space.png"/>
+            <img id='homeDisplay' alt="world" src="https://raw.githubusercontent.com/Group-17-COP4331-Summer-2020-UCF/PT3/master/app/img/Space.png"/>
         </div> 
     );
 };
