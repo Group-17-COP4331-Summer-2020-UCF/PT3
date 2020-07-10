f47e25c372a9b60a604bd1dfae959e5f999b797e
styler-collab <59663629+styler-collab@users.noreply.github.com>
2020-07-10 02:16:12 -0400

    Create Dashboard.js

---
+++ backend/frontend/src/components/dashboard/Dashboard.js
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../../css/dashboard/Dashboard.css';
+import React, { useState } from 'react';
+
+const BASE_URL = 'https://large-project-2020.herokuapp.com/';
+
+function Dashboard()
+{
+    
+    const doLogout = async event => 
+    {
+        event.preventDefault();
+
+        deleteCookie();
+        window.location.href = window.location.hostname; 
+    };
+
+    return(
+        <div className="col-sm-2 countainer-fluid full-height" id="loginField">
+            <h1 id="loginHeader">
+                PT3
+            </h1>
+            <div id="logoutDiv">
+                <input type="button" id="logoutButton" className="btn btn-light" value="Login" onClick={doLogout} />
+            </div>
+        </div>
+    );
+};
+
+function deleteCookie(username, password, name)
+{
+	document.cookie = "cookiename= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+}
