833614da340ea96aca740e6a3fa414486a6a558c
styler-collab <59663629+styler-collab@users.noreply.github.com>
2020-07-09 22:51:33 -0400

    Update DashboardPage.js

--- backend/frontend/src/pages/DashboardPage.js
+++ backend/frontend/src/pages/DashboardPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../components/general/Footer';
 import Navigation from '../components/general/Navigation';
+import Dashboard from '../components/dashboard/Dashboard';
 
 function DashboardPage() {
     return(
