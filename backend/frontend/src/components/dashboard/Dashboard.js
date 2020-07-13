743a5eebf08af0ad039e3fcbdc9afe90bec188aa
JaimeBR-ui <bambas@knights.ucf.edu>
2020-07-13 19:26:31 -0400

    Restructured Dashboard.

--- backend/frontend/src/components/dashboard/Dashboard.js
+++ backend/frontend/src/components/dashboard/Dashboard.js
@@ -1,16 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../css/dashboard/Dashboard.css';
-import React from 'react';
+import React, { Profiler } from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
+import Profile from './subComponents/Profile'
+import Customizer from './subComponents/Customizer'
+import DataGrapher from './subComponents/DataGrapher'
+import Test from './subComponents/Test'
 
 // const BASE_URL = 'https://large-project-2020.herokuapp.com/';
 
 function Dashboard()
 {
-    return(
-        <div className="dashboard">
-            <h1 id="loginHeader">
-                PT3
-            </h1>
+    // this is currently a prototype page of the layout of the data.
+    return(   
+        <div id="dashboard-container">
+            <Row id="trainer-row">
+                <Col id="user-cluster" className="column-hover-custom" lg={3}>
+                    <Profile/>
+                    <Customizer/>
+                </Col>
+                <Col id="history-cluster" className="column-hover-custom" lg={4}>
+                    <DataGrapher/>
+                </Col>
+                <div id="test-cluster" className="column-hover-custom">
+                    <Test/>
+                </div>
+            </Row>
         </div>
     );
 };
