4b0e758286356c5c21b49e426db4f298e3907ed0
styler-collab <59663629+styler-collab@users.noreply.github.com>
2020-07-20 16:19:31 -0400

    customizer working version

--- backend/frontend/src/components/dashboard/subComponents/Customizer.js
+++ backend/frontend/src/components/dashboard/subComponents/Customizer.js
@@ -1,65 +1,73 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../../css/dashboard/subComponents/Customizer.css';
 import React from 'react';
-import { Row, Col } from 'react-bootstrap'
-
-// const BASE_URL = 'https://large-project-2020.herokuapp.com/';
+import { Row, Col, Button } from 'react-bootstrap'
+import Cookie from '../../general/Cookie'
+const BASE_URL = 'https://large-project-2020.herokuapp.com/';
 
 function Customizer() {
-	const doChange = async event =>
-	{ //insert change gender/age here
-	};
-    // this is currently a prototype page of the layout of the data.
-    return (
-        <Col id="customizer-wrapper" fluid>
-            <h1>
-                Select Sex/Age groups!
+  var age;
+  const setMale = async event => {
+    Cookie.saveCookie("sex", "male")
+  };
+  const setFemale = async event => {
+    Cookie.saveCookie("sex", "female")
+  };
+  const setAge = async event => {
+    age = document.getElementById("ageInput").value;
+    Cookie.saveCookie("age", age);
+  };
+  const setArmy = async event => {
+    Cookie.saveCookie("test", "army");
+  };
+  const setNavy = async event => {
+    Cookie.saveCookie("test", "navy");
+  };
+  const setMarine = async event => {
+    Cookie.saveCookie("test", "marine");
+  };
+  const setAirforce = async event => {
+    Cookie.saveCookie("test", "airforce");
+  };
+
+  // this is currently a prototype page of the layout of the data.
+  return (
+    <Col id="customizer-wrapper" fluid>
+      <h1>
+        Select Sex/Age groups!
             </h1>
-	    <Row id="dropDown">
-                <div class="dropdown">
-			<button onclick="ageDrop()" class="dropbtn">Dropdown</button>
-			<div id="ageDropdown" class="dropdown-content">
-				<a href="#17-21">17-21</a>
-				<a href="#22-26">22-26</a>
-				<a href="#27-31">27-31</a>
-				<a href="#32-36">32-36</a>
-				<a href="#37-41">37-41</a>
-			</div>
-		</div>
-                <div class="dropdown">
-			<button onclick="sexDrop()" class="dropbtn">Dropdown</button>
-			<div id="sexDropdown" class="dropdown-content">
-				<a href="#male">Male</a>
-				<a href="#female">Female</a>
-			</div>
-		</div>
-            </Row>
-            <div className="button-gradient">
-                <input type="button" id="groupButton" className="btn btn-light" value="Submit" onClick={doChange} />
-            </div>
-        </Col>
-    );
+      <Row>
+        <div>
+          <input type="button" id="groupButton" className="btn btn-light" value="Male" onClick={setMale} />
+        </div>
+        <div>
+          <input type="button" id="groupButton" className="btn btn-light" value="Female" onClick={setFemale} />
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <input type="button" id="groupButton" className="btn btn-light" value="Army" onClick={setArmy} />
+        </div>
+        <div>
+          <input type="button" id="groupButton" className="btn btn-light" value="Navy" onClick={setNavy} />
+        </div>
+        <div>
+          <input type="button" id="groupButton" className="btn btn-light" value="Marine" onClick={setMarine} />
+        </div>
+        <div>
+          <input type="button" id="groupButton" className="btn btn-light" value="Air Force" onClick={setAirforce} />
+        </div>
+      </Row>
+      <Row>
+        <div>
+        <input type="email" className="form-control" id="ageInput" placeholder="Enter age" /><br />
+        </div>
+        <div>
+          <input type="button" id="ageButton" className="btn btn-light" value="Set age" onClick={setAge} />
+        </div>
+      </Row>
+    </Col>
+  );
 };
-
-function ageDrop() {
-  document.getElementById("ageDropdown").classList.toggle("show");
-}
-function sexDrop() {
-  document.getElementById("sexDropdown").classList.toggle("show");
-}
-
-//hide dropdown
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 
 export default Customizer;
