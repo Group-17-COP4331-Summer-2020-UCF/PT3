970e909520ec44486ef1ae1f50ffd238614893e3
JaimeBR-ui <bambas@knights.ucf.edu>
2020-07-20 22:36:07 -0400

    Updated about page.

--- backend/frontend/src/pages/AboutPage.js
+++ backend/frontend/src/pages/AboutPage.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import Footer from '../components/general/Footer';
 import Navigation from '../components/general/Navigation';
+import '../css/general/aboutPage.css'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function AboutPage() {
     return (
         <div>
             <Navigation/>
-            Currently in development! come back later!
+            <div id="about-wrapper">
+                <Row>
+                    <Col>
+                        <h4> Mobile </h4>
+                    </Col>
+                    <Col>
+                        <Col> Sebastian Fabara </Col>
+                        <Col> Michael Dews </Col>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <h4>  Web Application </h4>
+                    </Col>
+                    <Col>
+                        <Col> Jaime Bohorquez </Col>
+                        <Col> Tyler Sanders </Col>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h4> API </h4>
+                    </Col>
+                    <Col>
+                        <Col> Brandon Broomell </Col>
+                    </Col>
+                </Row>
+            </div>
             <Footer/>
         </div>
     );
