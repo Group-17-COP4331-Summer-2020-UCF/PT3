64766d7502e3a7f3916c1566804af1a672350686
JaimeBR-ui <bambas@knights.ucf.edu>
2020-07-22 04:13:07 -0400

    Connected verify page to API.

--- backend/frontend/src/pages/VerifyPage.js
+++ backend/frontend/src/pages/VerifyPage.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import Tour from '../components/login/Tour'
-import Login from '../components/login/Login';
-import Footer from '../components/general/Footer';
+import '../css/general/help.css'
 import Navigation from '../components/general/Navigation';
+import Footer from '../components/general/Footer';
 import { Container, Row, Col, Button } from 'react-bootstrap'
 
-import '../css/general/help.css'
+const BASE_URL = 'https://large-project-2020.herokuapp.com/';
+
 function VerifyPage() {
     const doVerify = async event => {
         event.preventDefault();
+        var token = document.getElementById("tokenInput").value;
+        var js = '{"token":"'
+            +  token + '"}';
+
+        try
+        {
+            document.getElementById("tokenInput").innerHTML = "Logging in...";
+            const response = await fetch(BASE_URL + 'users/verifyUser',
+                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+            var res = JSON.parse(await response.text());
+
+            console.log(res);
+            if (res != null && res.error != null)
+            {
+                alert("Invalid or expired token")
+                return;
+            }
+            else
+            {
+                // redirect
+                window.location.href = '/dashboard';
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }    
     }
     return(
         <div id="dashboard">
@@ -17,7 +46,7 @@ function VerifyPage() {
             <Col id="help-wrapper">
                 <br />
                 <h3> Enter your token </h3>
-                <input type="text" className="form-control" id="registerUser" placeholder="Token (sent to your email)" /><br />
+                <input type="text" className="form-control" id="tokenInput" placeholder="Token (sent to your email)" /><br />
                     <Button id="submit-button" variant="info" onClick={doVerify}> Submit </Button>
                 
                 <br />
