69c8212d5069538b2b9a1f2c6d32b5deb7fbce23
styler-collab <59663629+styler-collab@users.noreply.github.com>
2020-07-22 19:28:20 -0400

    bargraph added

--- backend/frontend/src/components/dashboard/subComponents/DataGrapher.js
+++ backend/frontend/src/components/dashboard/subComponents/DataGrapher.js
@@ -2,13 +2,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../../../css/dashboard/subComponents/DataGrapher.css';
 import React, { Component } from 'react';
 //import { Row } from 'react-bootstrap'
-import { Pie } from 'react-chartjs-2';
+import { Pie, Bar } from 'react-chartjs-2';
 import Cookie from '../../general/Cookie'
 
 const BASE_URL = 'https://large-project-2020.herokuapp.com/';
 
 var passCount = 0, failCount = 0;
-var state;
+var barData = {
+    labels: ['Situp Goal', 'Situp Avg.', 'Pullup Goal', 'Pullup Avg.', 'Run Goal', 'Run Avg.'],
+    datasets: [
+        {
+            backgroundColor: ['#824ce6', '#824ce6', 'blue', 'blue', '#00ab66', '#00ab66'],
+            borderColor: '#000000',
+            borderWidth: 2,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: [35, 20, 47, 60, 12.30, 15.30]
+        }
+    ]
+};
 class DataGrapher extends React.Component {
     constructor(props) {
         super(props);
@@ -28,17 +40,16 @@ class DataGrapher extends React.Component {
         };
     }
 
-    updateData = () =>{
-        this.state.data = [Cookie.getCookie("passCount"), Cookie.getCookie("failCount")];
+    updateData = () => {
         window.location.reload();
     }
 
     componentDidUpdate(prevProps, prevState) {
         // check whether client has changed
         if (prevState.data !== this.state.data) {
-          this.fetchData(this.state.data);
+            this.fetchData(this.state.data);
         }
-      }
+    }
 
     render() {
         return (
@@ -69,6 +80,34 @@ class DataGrapher extends React.Component {
                     }}
                 />
                 <button onClick={this.updateData}>Update Tests</button>
+                <Bar
+                    data={barData}
+                    width={100}
+                    height={50}
+                    options={{
+                        maintainAspectRatio: true,
+                        legend: {
+                            display: false
+                        },
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    fontColor: "black",
+                                    fontSize: 18,
+                                    stepSize: 1,
+                                    beginAtZero: true
+                                }
+                            }],
+                            xAxes: [{
+                                ticks: {
+                                    fontColor: "black",
+                                    fontSize: 14,
+                                    stepSize: 1,
+                                }
+                            }]
+                        }
+                    }}
+                />
             </div>
 
         );
